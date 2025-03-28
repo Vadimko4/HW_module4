d48f588dcdfc183bd84c203cb55e65a5b90c3765
vadimko4 <vad.informat@gmail.com>
2025-03-28 15:28:26 +0400

    update utils create_objects_from_dicts_list

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 
 import json
+from src.product import Product
 from src.category import Category
 
 
@@ -11,3 +12,26 @@ def read_categories_dict_from_json(path: str) -> list[dict]:
     return dicts_list
 
 
+def create_objects_from_dicts_list(data_list) -> list[Category]:
+    categories_list = []
+    for item in data_list:
+        products_list = []
+        for i in item.get('products'):
+            product = Product(**i)
+            # Product(i.get('name'), i.get('description'), i.get('price'), i.get('quantity'))
+            products_list.append(product)
+        item['products'] = products_list
+        # category = Category(item.get('name'), item.get('descriptiondescription'), products_list)
+        categories_list.append(Category(**item))
+
+    return categories_list
+
+
+if __name__ == "__main__":
+    dicts = read_categories_dict_from_json('../data/products.json')
+    categories = create_objects_from_dicts_list(dicts)
+    print(categories)
+    print(categories[0].name)
+    print(categories[0].products)
+    print(categories[0].categories_quantity)
+    print(categories[0].products_quantity)
