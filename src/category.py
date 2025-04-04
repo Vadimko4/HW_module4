af3a5bce9c26e6702af344a34dd649c60e1a2fe9
vadimko4 <vad.informat@gmail.com>
2025-04-05 00:22:14 +0400

    update category

--- src/category.py
+++ src/category.py
@@ -15,7 +15,7 @@ class Category:
         self.__products = products
 
         Category.categories_quantity += 1
-        Category.product_count += len(products)
+        Category.product_count += sum(product.quantity for product in products)
 
     def __str__(self):
         return f"{self.name}, количество продуктов: {self.product_count} шт."
