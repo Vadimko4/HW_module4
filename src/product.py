426b049a2b5ef375bb2329f5702ee8cdd2be9025
vadimko4 <vad.informat@gmail.com>
2025-04-06 22:00:12 +0400

    update product add subclasses with atributes

--- src/product.py
+++ src/product.py
@@ -54,3 +54,22 @@ class Product:
                 self.__price = new_price
         else:
             print("Цена не должна быть нулевая или отрицательная")
+
+
+class Smartphone(Product):
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
