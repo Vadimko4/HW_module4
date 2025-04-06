faa87ab10525a369e9c20d499e0bc40839efe1a4
vadimko4 <vad.informat@gmail.com>
2025-04-07 00:05:58 +0400

    after linters

--- src/category.py
+++ src/category.py
@@ -1,4 +1,5 @@
 from typing import Any
+
 from src.product import Product
 
 
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def test_product_init(first_test_product, second_test_product):
     assert first_test_product.name == "Samsung Galaxy C23 Ultra"
     assert first_test_product.description == "256GB, Серый цвет, 200MP камера"
@@ -30,8 +31,8 @@ def test_product_price_setter(first_test_product):
     assert first_test_product.price == 180000.0
     first_test_product.price = 185000.0
     assert first_test_product.price == 185000.0
-    
-    
+
+
 def test_first_smartphone_init(test_first_smartphone):
     assert test_first_smartphone.name == "Samsung Galaxy S23 Ultra"
     assert test_first_smartphone.description == "256GB, Серый цвет, 200MP камера"
