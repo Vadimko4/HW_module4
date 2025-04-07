34d3d2e56aa74cb1db244cc71790e150298d019b
vadimko4 <vad.informat@gmail.com>
2025-04-07 13:22:53 +0400

    add print_mixin

---
+++ src/print_mixin.py
@@ -0,0 +1,8 @@
+class PrintMixin:
+
+    def __init__(self, *args, **kwargs):
+        print(self.__repr__(self, args, kwargs))
+
+    def __repr__(self, *args, **kwargs):
+        return f"{self.__class__.__name__}, {kwargs}"
+    # от какого класса и с какими параметрами был создан объект.
