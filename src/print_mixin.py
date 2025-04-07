940c9916a24ff75436f48e44e63d824384a5b349
vadimko4 <vad.informat@gmail.com>
2025-04-07 14:26:02 +0400

    update product, main

--- src/print_mixin.py
+++ src/print_mixin.py
@@ -1,8 +1,8 @@
 class PrintMixin:
 
-    def __init__(self, *args, **kwargs):
-        print(self.__repr__(self, args, kwargs))
+    def __init__(self):
+        print(repr(self))
 
-    def __repr__(self, *args, **kwargs):
-        return f"{self.__class__.__name__}, {*args, }, {*kwargs, }"
+    def __repr__(self):
+        return f"{self.__class__.__name__}, {', '.join(str(i) for i in self.__dict__.values())}"
     # от какого класса и с какими параметрами был создан объект.
--- src/product.py
+++ src/product.py
@@ -2,7 +2,7 @@ from src.baseproduct import BaseProduct
 from src.print_mixin import PrintMixin
 
 
-class Product(BaseProduct, PrintMixin):
+class Product(PrintMixin, BaseProduct):
     name: str
     description: str
     __price: float
@@ -15,7 +15,7 @@ class Product(BaseProduct, PrintMixin):
         self.description = description
         self.__price = price
         self.quantity = quantity
-        super().__init__(name, description, price, quantity)
+        super().__init__()
         Product.products_list.append(self)
 
     def __str__(self):
@@ -89,3 +89,14 @@ class LawnGrass(Product):
             return self.price * self.quantity + other.price * other.quantity
         else:
             raise TypeError
+
+
+if __name__ == '__main__':
+    print(Product.__mro__)
+    product1 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+    phone = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера",
+               180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
+
+    grass = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0,
+                     20, "Россия", "7 дней", "Зеленый")
